40b3577ba8ce2ddd2a6d8b0908d51a5ca396b8de
Grace Lavin <gcl5087@psu.edu>
2020-08-28 13:31:14 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+
+celsiusV = input("Enter temperature in celsius: ")
+
+
+celsiusV = float(celsiusV)
+
+fahrV = (1.8 *(celsiusV) + 32)
+
+print( str(celsiusV) + "° in Celsius is equivalent to " + str(fahrV) + "° Fahrenheit")
